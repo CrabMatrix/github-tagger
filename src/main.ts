ce769de10d155630c3cb44b3e909f2514a84304c
nate <natecaple@gmail.com>
2022-11-22 17:36:44 -0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run() {
+  try {
+    const token = core.getInput('repo-token', {required: true});
+    const tag = core.getInput('tag', {required: true});
+    const sha = core.getInput('commit-sha', {required: false}) || github.context.sha;
+
+    const client = new github.GitHub(token);
+
+    core.debug(`tagging #${sha} with tag ${tag}`);
+    await client.git.createRef({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      ref: `refs/tags/${tag}`,
+      sha: sha
+    });
+
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
