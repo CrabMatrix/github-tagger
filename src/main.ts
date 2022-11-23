9d03be34917a31a9d962be8cffbe4cceb374dcf6
nate <natecaple@gmail.com>
2022-11-23 14:54:42 -0600

    .

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+import github from '@actions/github';
+import core from '@actions/core';
 
 async function run() {
   try {
@@ -7,38 +7,35 @@ async function run() {
     const tag = core.getInput("tag", { required: true });
     const sha = core.getInput("commit-sha", { required: false }) || github.context.sha;
 
-    const client = new github.GitHub(token);
+    const client = github.getOctokit(token);
+
     core.debug(`tagging #${sha} with tag ${tag}`);
 
-    let list = await client.git.listRefs({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo
-    });
-
-    core.debug(`list: #${list.data}`);
-
     try {
-      await client.git.createRef({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        ref: `refs/tags/${tag}`,
-        sha: sha
-      });
+      await client.graphql(`mutation {
+        createRef(input: {repositoryId: "${github.context.repo.repo}", name: "refs/tags/${tag}", oid: "${sha}"}) {
+          ref {
+            id
+          }
+        }
+      }`);
       core.debug(`Created tag ${tag} for commit ${sha}`);
-    } catch (error) {
-      core.debug(`error: #${error}`);
-      await client.git.updateRef({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        ref: `tags/${tag}`,
-        sha: sha,
-        force: true
-      });
+    } catch (err) {
+      core.debug(`error: #${err}`);
+
+      await client.graphql(`mutation {
+        updateRef (input: {repositoryId: "${github.context.repo.repo}", name: "refs/tags/${tag}", oid: "${sha}"}) {
+          ref {
+            id
+          }
+        }
+      }`);
+
       core.debug(`Updated tag ${tag} for commit ${sha}`);
     }
-  } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
+  } catch (err) {
+    core.error(err);
+    core.setFailed(err.message);
   }
 }
 
