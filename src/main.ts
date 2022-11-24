4e2035ae0fdb817da8bbb6015aa7ef7bff839442
nate <natecaple@gmail.com>
2022-11-23 20:19:54 -0600

    .

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,38 @@
-import github from '@actions/github';
-import core from '@actions/core';
+import github from "@actions/github";
+import core from "@actions/core";
 
 async function run() {
-  try {
-    const token = core.getInput("repo-token", { required: true });
-    const tag = core.getInput("tag", { required: true });
-    const sha = core.getInput("commit-sha", { required: false }) || github.context.sha;
+  const token: string = core.getInput("repo-token", { required: true });
+  const tag: string = core.getInput("tag", { required: true });
+  const sha: string =
+    core.getInput("commit-sha", { required: false }) || github.context.sha;
 
-    const client = github.getOctokit(token);
+  const client = github.getOctokit(token);
 
-    const refs = await client.rest.git.listMatchingRefs({
+  const refs = await client.rest.git.listMatchingRefs({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    ref: `tags/${tag}`
+  });
+
+  if (refs.data.length > 0) {
+    core.info(`Overwriting ${tag} with ${sha}`);
+    await client.rest.git.updateRef({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      ref: `tags/${tag}`
+      ref: `refs/tags/${tag}`,
+      sha: sha,
+      force: true
     });
-
-    if (refs.data.length > 0) {
-      core.info(`Overwriting ${tag} with ${sha}`);
-      await client.rest.git.updateRef({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        ref: `refs/tags/${tag}`,
-        sha: sha,
-        force: true
-      });
-    } else {
-      core.info(`Creating tag ${tag} for commit ${sha}`);
-      await client.rest.git.createRef({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        ref: `refs/tags/${tag}`,
-        sha: sha
-      });
-    }
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+  } else {
+    core.info(`Creating tag ${tag} for commit ${sha}`);
+    await client.rest.git.createRef({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      ref: `refs/tags/${tag}`,
+      sha: sha
+    });
   }
 }
 
-run();
+run().catch(error => core.setFailed("Workflow failed! " + error.message));
